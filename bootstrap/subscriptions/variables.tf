4308abdd252f3c0dd88c1ba08b6acd11fe850a1f
Chris Schreiber <chriss@liatrio.com>
2022-02-14 12:22:11 -0800

    feat: add subscriptions bootrstrap

---
+++ bootstrap/subscriptions/variables.tf
@@ -0,0 +1,18 @@
+variable "group_prefix" {
+  type = string
+  default = "caf"
+}
+
+variable "billing_account_name" {
+  type = string  
+}
+
+variable "billing_profile_name" {
+    type = string
+
+}
+
+variable "invoice_section_name" {
+  type = string
+  
+}
