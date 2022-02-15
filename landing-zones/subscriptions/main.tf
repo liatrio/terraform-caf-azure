cafd4f635d70f2299fa1adbda6cd77a1dfca345e
Calvin Wai <cwai@ip-192-168-1-14.ec2.internal>
2022-02-15 12:56:09 -0800

    feat: add landing zones subscription module

---
+++ landing-zones/subscriptions/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.96"
+    }
+  }
+}
+
+data "azurerm_billing_mca_account_scope" "billing" {
+  billing_account_name = var.billing_account_name
+  billing_profile_name = var.billing_profile_name
+  invoice_section_name = var.invoice_section_name
+}
+
+resource "azurerm_subscription" "landing_zone" {
+  subscription_name = "${var.name}"
+  billing_scope_id  = data.azurerm_billing_mca_account_scope.billing.id
+
+  tags = {}
+}
