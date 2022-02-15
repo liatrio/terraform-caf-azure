d2ac4e87b5b105cb94194f79a3c9a3f84e3689b8
Calvin Wai <cwai@ip-192-168-1-14.ec2.internal>
2022-02-15 12:20:20 -0800

    fix: change output value for aks module

--- modules/aks/outputs.tf
+++ modules/aks/outputs.tf
@@ -1,7 +1,6 @@
 output "cluster_name" {
-  value = var.cluster_name
+  value = azurerm_kubernetes_cluster.main.name
 }
 
 output "resource_group_name" {
-  value = var.resource_group_name
-}
+  value = var.azurerm_resource_group.aks.name
