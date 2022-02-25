09a118c703a23917b48e6c76c23d82f50418e8c9
cjobee <95240635+cjobee@users.noreply.github.com>
2022-02-25 17:07:42 -0600

    feat: root public dns zone (#15)
    * feat: add public dns root zone
    
    * chore: updated root_dns var
    
    * chore: updated public dns module
    
    * Update modules/public-dns-zones/variables.tf
    
    Co-authored-by: steveno@liatrio <75693326+steve-liatrio@users.noreply.github.com>
    
    * Update modules/public-dns-zones/variables.tf
    
    Co-authored-by: steveno@liatrio <75693326+steve-liatrio@users.noreply.github.com>
    
    * chore: terraform fmt
    
    Co-authored-by: steveno@liatrio <75693326+steve-liatrio@users.noreply.github.com>

---
+++ modules/public-dns-zones/variables.tf
@@ -0,0 +1,15 @@
+variable "location" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "root_dns_zone" {
+  type = string
+}
+
+variable "tags" {
+  default = {}
+}
