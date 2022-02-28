abad2403b9d1a944e0b1f898e65bd851b899434f
Chris Warren <chrisw@liatrio.com>
2022-02-28 09:32:25 -0600

    refactor: replace locals within module scope for private dns zones

--- foundation/connectivity/dns.tf
+++ foundation/connectivity/dns.tf
@@ -1,10 +1,19 @@
+locals {
+  # map of azurerm services to fqdns for private dns zones
+  # keys are named after the terraform resources that these private dns zones would be used for
+  azure_paas_private_dns_zones = {
+    kubernetes_cluster = "privatelink.${var.location}.azmk8s.io"
+    container_registry = "privatelink.azurecr.io"
+  }
+}
+
 module "private_dns" {
   source = "../../modules/private-dns-zones"
 
   location                     = azurerm_point_to_site_vpn_gateway.hub_vpn_gateway.location
   resource_group_name          = azurerm_resource_group.caf_connectivity.name
   linked_virtual_network_id    = azurerm_virtual_network.connectivity_vnet.id
-  azure_paas_private_dns_zones = var.private_dns_zones
+  azure_paas_private_dns_zones = local.azure_paas_private_dns_zones
 }
 
 module "public_dns" {
