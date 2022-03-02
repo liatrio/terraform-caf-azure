96bd257773b2f8277275fc1511cf994d1bb6f5aa
Chris Warren <chrisw@liatrio.com>
2022-03-02 17:39:46 -0600

    chore: tf fmt update

--- foundation/shared-services/core/variables.tf
+++ foundation/shared-services/core/variables.tf
@@ -54,16 +54,16 @@ variable "kubernetes_version" {
 }
 
 variable "parent_dns_zone_name" {
-  type = string
+  type        = string
   description = "Optional parent DNS zone name which causes a child zone to be created"
 }
 
 variable "connectivity_resource_group_name" {
-  type = string
+  type        = string
   description = "Connectivity resource group name"
 }
 
 variable "public_dns_zone_name" {
-  type = string
+  type        = string
   description = "public dns zone to create"
 }
