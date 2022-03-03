4304a6c935e3c8e39aba2584266a06caf8030feb
Micah Perez <micahperez99@gmail.com>
2022-03-03 14:20:25 -0800

    fix: add subscript operator to module namespace reference

--- foundation/shared-services/applications/monitoring.tf
+++ foundation/shared-services/applications/monitoring.tf
@@ -7,7 +7,7 @@ resource "kubernetes_namespace" "monitoring" {
 module "kube_prometheus_stack" {
   source = "../../../modules/kube-prometheus-stack"
 
-  namespace           = kubernetes_namespace.monitoring.metadata.name
+  namespace           = kubernetes_namespace.monitoring.metadata[0].name
   enable_grafana      = true
   enable_alertmanager = true
 }
