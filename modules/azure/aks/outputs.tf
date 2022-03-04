7d398501f30ea08a396dea51cc278d899e37cd2a
Steven O <steveno@liatrio.com>
2022-03-04 11:51:33 -0600

    fmt

--- modules/azure/aks/outputs.tf
+++ modules/azure/aks/outputs.tf
@@ -22,9 +22,9 @@ output "cluster_ca_certificate" {
 }
 
 output "kubelet_identity_client_id" {
-  value     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].client_id
+  value = azurerm_kubernetes_cluster.aks.kubelet_identity[0].client_id
 }
 
 output "kubelet_identity_object_id" {
-  value     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  value = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
 }
