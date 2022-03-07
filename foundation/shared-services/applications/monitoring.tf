29d586d4ca5a8928e8d5657f12ca8c71e634c9e8
Micah Perez <micahperez99@gmail.com>
2022-03-07 11:27:55 -0800

    refactor: add parameters to prometheus stack module in shared services

--- foundation/shared-services/applications/monitoring.tf
+++ foundation/shared-services/applications/monitoring.tf
@@ -7,7 +7,13 @@ resource "kubernetes_namespace" "monitoring" {
 module "kube_prometheus_stack" {
   source = "../../../modules/kube-prometheus-stack"
 
-  namespace           = kubernetes_namespace.monitoring.metadata[0].name
-  enable_grafana      = true
-  enable_alertmanager = true
+  namespace                        = kubernetes_namespace.monitoring.metadata[0].name
+  enable_grafana                   = true
+  enable_alertmanager              = true
+  grafana_hostname                 = ""
+  alertmanager_hostname            = ""
+  prometheus_slack_webhook_url     = ""
+  prometheus_slack_channel         = ""
+  grafana_ingress_annotations      = ""
+  alertmanager_ingress_annotations = ""
 }
