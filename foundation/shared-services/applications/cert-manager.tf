72844ef734db984d2b1a20888ce45c49f1fe6acf
Chris Warren <chrisw@liatrio.com>
2022-03-08 10:31:41 -0600

    chore: move cert manager pod identity creation to cert manager file

--- foundation/shared-services/applications/cert-manager.tf
+++ foundation/shared-services/applications/cert-manager.tf
@@ -1,3 +1,14 @@
+module "cert_manager_pod_identity" {
+  depends_on = [module.aad_pod_identity]
+  source     = "../../../modules/kubernetes/aad-pod-identity-instance"
+
+  namespace = kubernetes_namespace.toolchain_namespace.metadata.0.name
+
+  identity_name        = "cert-manager-pod-identity"
+  identity_client_id   = var.aad_pod_identity_client_id
+  identity_resource_id = var.aad_pod_identity_resource_id
+}
+
 module "cert_manager" {
   source = "../../../modules/kubernetes/cert-manager"
 
--- foundation/shared-services/applications/main.tf
+++ foundation/shared-services/applications/main.tf
@@ -10,14 +10,3 @@ resource "kubernetes_namespace" "toolchain_namespace" {
 module "aad_pod_identity" {
   source = "../../../modules/kubernetes/aad-pod-identity"
 }
-
-module "cert_manager_pod_identity" {
-  depends_on = [module.aad_pod_identity]
-  source     = "../../../modules/kubernetes/aad-pod-identity-instance"
-
-  namespace = kubernetes_namespace.toolchain_namespace.metadata.0.name
-
-  identity_name        = "cert-manager-pod-identity"
-  identity_client_id   = var.aad_pod_identity_client_id
-  identity_resource_id = var.aad_pod_identity_resource_id
-}
