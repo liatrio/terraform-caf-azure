bea9a4b608adbeac2a04a655ce19b461a77d9681
Chris Warren <chrisw@liatrio.com>
2022-03-08 11:14:38 -0600

    chore: simplify validation to require exactly 16 bit vnet cidr

--- modules/azure/aks-vnet/variables.tf
+++ modules/azure/aks-vnet/variables.tf
@@ -12,8 +12,8 @@ variable "vnet_address_range" {
   description = "The CIDR expression (e.g. '10.0.0.0/8') giving an IPv4 network address range for the vnet. Should be a /16 or larger to allow room for AKS cluster operations using Azure CNI"
   type        = string
   validation {
-    condition     = split("/", var.vnet_address_range)[1] <= 16
-    error_message = "The vnet_address_range must be at least size /16 (i.e. must use no more than 16 network bits)."
+    condition     = cidrnetmask(var.vnet_address_range) == "255.255.0.0"
+    error_message = "The vnet_address_range must be a /16 (i.e. Subnet mask of 255.255.0.0)."
   }
 }
 
