bcd5121f690e2451492051f617bde4b4b3828ba9
chodges7 <cdhodges5@gmail.com>
2022-03-08 08:52:23 -0800

    fix: disable grafana and alertmanager until nginx deployment

--- foundation/shared-services/applications/monitoring.tf
+++ foundation/shared-services/applications/monitoring.tf
@@ -8,8 +8,8 @@ module "kube_prometheus_stack" {
   source = "../../../modules/kube-prometheus-stack"
 
   namespace                        = kubernetes_namespace.monitoring.metadata[0].name
-  enable_grafana                   = true
-  enable_alertmanager              = true
+  enable_grafana                   = false
+  enable_alertmanager              = false
   grafana_hostname                 = "placeholder-domain"
   alertmanager_hostname            = "placeholder-domain"
   prometheus_slack_webhook_url     = ""
