14dd66f861a3173261b5a18fc6f5d3f2eff6e48c
Chris Warren <chrisw@liatrio.com>
2022-03-08 09:43:27 -0600

    refactor: move cert-manager tf to separate file

---
+++ foundation/shared-services/applications/cert-manager.tf
@@ -0,0 +1,43 @@
+module "cert_manager" {
+  source = "../../../modules/kubernetes/cert-manager"
+
+  namespace    = kubernetes_namespace.toolchain_namespace.metadata.0.name
+  pod_identity = module.cert_manager_pod_identity.identity_name
+}
+
+resource "time_sleep" "wait_for_cert_manager" {
+  depends_on = [module.cert_manager]
+
+  create_duration = "20s"
+}
+
+module "cert_manager_issuer" {
+  depends_on = [
+    time_sleep.wait_for_cert_manager,
+    module.cert_manager_pod_identity
+  ]
+  source = "../../../modules/kubernetes/cert-manager-issuer"
+
+  namespace                    = kubernetes_namespace.toolchain_namespace.metadata.0.name
+  issuer_name                  = var.issuer_name
+  issuer_server                = var.issuer_server
+  issuer_email                 = var.issuer_email
+  azure_subscription_id        = var.azure_subscription_id
+  dns_zone_resource_group_name = var.dns_zone_resource_group_name
+  dns_zone_name                = var.dns_zone_name
+}
+
+module "cluster_wildcard" {
+  depends_on = [
+    module.cert_manager,
+    module.cert_manager_issuer,
+    module.cert_manager_pod_identity
+  ]
+  source = "../../../modules/kubernetes/certificates"
+
+  name      = "cluster-wildcard"
+  namespace = var.namespace
+  domain    = var.dns_zone_name
+
+  issuer_name = var.issuer_name
+}
