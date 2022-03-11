dc752921b4603bec3f1e8d9599f42c2f09869942
steveno@liatrio <75693326+steve-liatrio@users.noreply.github.com>
2022-03-11 16:26:42 -0600

    typo
    Co-authored-by: Michael Parker <michael@parker.gg>

--- modules/azure/aks-vnet/locals-cidr-calculations.tf
+++ modules/azure/aks-vnet/locals-cidr-calculations.tf
@@ -1,6 +1,6 @@
 locals {
   # Takes in a /16 virtual network cidr range
-  # Assigns the first half (/17) of give cidr range to the pods and nodes subnet
+  # Assigns the first half (/17) of given cidr range to the pods and nodes subnet
   # Assigns the next quarter to the services subnet (todo: this should be a standard range for all clusters)
   # Assigns the second host available within the services subnet to the kube-dns discovery service
   # Assigns the next /21 for Azure private service endpoints
