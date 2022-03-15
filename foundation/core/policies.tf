08084c16cea733b41152fcbdf0a27fd4b2fd2c72
wai-calvin <calvinw@liatrio.com>
2022-03-15 15:18:51 -0700

    fix: tf fmt

--- foundation/core/policies.tf
+++ foundation/core/policies.tf
@@ -27,25 +27,25 @@ locals {
 module "foundation-policy-sets" {
   source                     = "../../modules/azure/policy-set-assignments-mg"
   target_management_group_id = azurerm_management_group.foundation.id
-  policy_sets             = var.default_policies_enabled ? concat(local.foundation_policy_sets, var.foundation_policy_sets) : var.foundation_policy_sets
+  policy_sets                = var.default_policies_enabled ? concat(local.foundation_policy_sets, var.foundation_policy_sets) : var.foundation_policy_sets
 }
 
 module "platform-policy-sets" {
   source                     = "../../modules/azure/policy-set-assignments-mg"
   target_management_group_id = azurerm_management_group.platform.id
-  policy_sets             = var.default_policies_enabled ? concat(local.platform_policy_sets, var.platform_policy_sets) : var.platform_policy_sets
+  policy_sets                = var.default_policies_enabled ? concat(local.platform_policy_sets, var.platform_policy_sets) : var.platform_policy_sets
 }
 
 module "landing_zones-policy-sets" {
   source                     = "../../modules/azure/policy-set-assignments-mg"
   target_management_group_id = azurerm_management_group.landing_zones.id
-  policy_sets             = var.default_policies_enabled ? concat(local.landing_zones_policy_sets, var.landing_zones_policy_sets) : var.landing_zones_policy_sets
+  policy_sets                = var.default_policies_enabled ? concat(local.landing_zones_policy_sets, var.landing_zones_policy_sets) : var.landing_zones_policy_sets
 }
 
 module "shared_svc-policy-sets" {
   source                     = "../../modules/azure/policy-set-assignments-mg"
   target_management_group_id = azurerm_management_group.shared_svc.id
-  policy_sets             = var.default_policies_enabled ? concat(local.shared_svc_policy_sets, var.shared_svc_policy_sets) : var.shared_svc_policy_sets
+  policy_sets                = var.default_policies_enabled ? concat(local.shared_svc_policy_sets, var.shared_svc_policy_sets) : var.shared_svc_policy_sets
 }
 
 module "policy-sets-dynamic-mgs" {
@@ -54,23 +54,23 @@ module "policy-sets-dynamic-mgs" {
   for_each = azurerm_management_group.dynamic
 
   target_management_group_id = each.value.id
-  policy_sets             = var.landing_zone_mg[each.key].policy_ids
+  policy_sets                = var.landing_zone_mg[each.key].policy_ids
 }
 
 module "connectivity-policy-sets" {
   source                     = "../../modules/azure/policy-set-assignments-mg"
   target_management_group_id = azurerm_management_group.connectivity.id
-  policy_sets             = var.default_policies_enabled ? concat(local.connectivity_policy_sets, var.connectivity_policy_sets) : var.connectivity_policy_sets
+  policy_sets                = var.default_policies_enabled ? concat(local.connectivity_policy_sets, var.connectivity_policy_sets) : var.connectivity_policy_sets
 }
 
 module "management-policy-sets" {
   source                     = "../../modules/azure/policy-set-assignments-mg"
   target_management_group_id = azurerm_management_group.management.id
-  policy_sets             = var.default_policies_enabled ? concat(local.management_policy_sets, var.management_policy_sets) : var.management_policy_sets
+  policy_sets                = var.default_policies_enabled ? concat(local.management_policy_sets, var.management_policy_sets) : var.management_policy_sets
 }
 
 module "identity-policy-sets" {
   source                     = "../../modules/azure/policy-set-assignments-mg"
   target_management_group_id = azurerm_management_group.identity.id
-  policy_sets             = var.default_policies_enabled ? concat(local.identity_policy_sets, var.identity_policy_sets) : var.identity_policy_sets
+  policy_sets                = var.default_policies_enabled ? concat(local.identity_policy_sets, var.identity_policy_sets) : var.identity_policy_sets
 }
--- modules/azure/policy-set-assignments-mg/main.tf
+++ modules/azure/policy-set-assignments-mg/main.tf
@@ -10,7 +10,7 @@ terraform {
 module "policy_assignment" {
   source = "./policy-set-assignment"
 
-  count = length(var.policy_sets)
+  count                      = length(var.policy_sets)
   target_management_group_id = var.target_management_group_id
   policy_set_id              = var.policy_sets[count.index].policy_set_id
   policy_parameters          = lookup(var.policy_sets[count.index], "parameters", null)
