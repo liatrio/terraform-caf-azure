427acfda0b2116d7c561896bc2589a063115b296
Matt Reynolds <mattreynolds@Matts-MacBook-Pro.local>
2022-03-24 15:29:49 -0500

    feat: added db conn string
    Signed-off-by: Matt Reynolds <mattreynolds@Matts-MacBook-Pro.local>

--- modules/azure/mysql-database/main.tf
+++ modules/azure/mysql-database/main.tf
@@ -16,6 +16,12 @@ resource "azurerm_key_vault_secret" "sql_pass" {
   key_vault_id = azurerm_key_vault.ss_kv.id
 }
 
+resource "azrurerm_keyvault_secret" "sql_conn_string" {
+  name         = "${azurerm_sql_database.sql_db.name}-conn-string"
+  value        = "mysql.createConnection({host: {${azurerm_sql_server.db_server.name}.mysql.database.azure.com}, user: {${azure_mysql_server.db_server.administrator_login}@${azurerm_sql_server.db_server.name}.mysql.database.azure.com}, password: {${random_password.sql_pass.result}}, database: {${azurerm_mysql_database.sql_db.name}, Port: {3306});"
+  key_vault_id = azurerm_key_vault.ss_kv.id
+}
+
 resource "azurerm_mysql_server" "db_server" {
   name                         = "${var.prefix}${var.environment}"
   location                     = var.location
@@ -57,59 +63,5 @@ resource "azurerm_mysql_database" "sql_db" {
 
   tags = {
     environment = var.environment
-  }
-}
-
-resource "azurerm_key_vault_certificate" "db_cert" {
-  name         = "generated-cert"
-  key_vault_id = data.azurerm_key_vault.ss_kv.id
-
-  certificate_policy {
-    issuer_parameters {
-      name = "Self"
-    }
-
-    key_properties {
-      exportable = true
-      key_size   = 2048
-      key_type   = "RSA"
-      reuse_key  = true
-    }
-
-    lifetime_action {
-      action {
-        action_type = "AutoRenew"
-      }
-
-      trigger {
-        days_before_expiry = 30
-      }
-    }
-
-    secret_properties {
-      content_type = "application/x-pkcs12"
-    }
-
-    x509_certificate_properties {
-      # Server Authentication = 1.3.6.1.5.5.7.3.1
-      # Client Authentication = 1.3.6.1.5.5.7.3.2
-      extended_key_usage = ["1.3.6.1.5.5.7.3.1"]
-
-      key_usage = [
-        "cRLSign",
-        "dataEncipherment",
-        "digitalSignature",
-        "keyAgreement",
-        "keyCertSign",
-        "keyEncipherment",
-      ]
-
-      subject_alternative_names {
-        dns_names = ["internal.contoso.com", "domain.hello.world"]
-      }
-
-      subject            = "CN=hello-world"
-      validity_in_months = 12
-    }
   }
 }
