551b949a38523b3e3f1416ddc883be73bf46e59c
Matt Reynolds <mattreynolds@Matts-MacBook-Pro.local>
2022-03-28 16:16:17 -0500

    cleaned up data ref, put in own data.tf file
    Signed-off-by: Matt Reynolds <mattreynolds@Matts-MacBook-Pro.local>

---
+++ modules/azure/mysql-database/data.tf
@@ -0,0 +1,20 @@
+data "azurerm_key_vault" "ss_kv" {
+  provider            = azurerm.shared_services
+  name                = "var.ss_kv"
+  resource_group_name = "var.ss_rg"
+}
+
+data "azurerm_resource_group" "vnet_rg" {
+  name   = var.resource_group_name
+}
+
+data "azurerm_virtual_network" "vnet" {
+  name                = var.name
+  resource_group_name = var.location
+}
+
+data "azurerm_subnet" "snet" {
+  name                 = "service-endpoints"
+  virtual_network_name = data.azurerm_virtual_network.vnet.name
+  resource_group_name  = data.azurerm_resource_group.vnet_rg.name
+}
--- modules/azure/mysql-database/main.tf
+++ modules/azure/mysql-database/main.tf
@@ -1,24 +1,3 @@
-data "azurerm_key_vault" "ss_kv" {
-  provider            = azurerm.shared_services
-  name                = "var.ss_kv"
-  resource_group_name = "var.ss_rg"
-}
-
-data "azurerm_resource_group" "vnet_rg" {
-  name   = var.resource_group_name
-}
-
-data "azurerm_virtual_network" "vnet" {
-  name                = var.name
-  resource_group_name = var.location
-}
-
-data "azurerm_subnet" "snet" {
-  name                 = "service-endpoints"
-  virtual_network_name = data.azurerm_virtual_network.vnet.name
-  resource_group_name  = data.azurerm_resource_group.vnet_rg.name
-}
-
 resource "random_password" "sql_pass" {
   length           = 16
   special          = true
