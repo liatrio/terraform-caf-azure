6177a5080487a6eb47413f7bf3f0a8e59f0d4b69
Matt Reynolds <mattreynolds@Matts-MacBook-Pro.local>
2022-03-28 16:14:43 -0500

    feat: added private endpoint and cert generation
    Signed-off-by: Matt Reynolds <mattreynolds@Matts-MacBook-Pro.local>

--- modules/azure/mysql-database/main.tf
+++ modules/azure/mysql-database/main.tf
@@ -4,6 +4,21 @@ data "azurerm_key_vault" "ss_kv" {
   resource_group_name = "var.ss_rg"
 }
 
+data "azurerm_resource_group" "vnet_rg" {
+  name   = var.resource_group_name
+}
+
+data "azurerm_virtual_network" "vnet" {
+  name                = var.name
+  resource_group_name = var.location
+}
+
+data "azurerm_subnet" "snet" {
+  name                 = "service-endpoints"
+  virtual_network_name = data.azurerm_virtual_network.vnet.name
+  resource_group_name  = data.azurerm_resource_group.vnet_rg.name
+}
+
 resource "random_password" "sql_pass" {
   length           = 16
   special          = true
@@ -63,5 +78,75 @@ resource "azurerm_mysql_database" "sql_db" {
 
   tags = {
     environment = var.environment
+  }
+}
+
+resource "azurerm_private_endpoint" "db-endpoint" {
+  name                = "${var.prefix}${var.environment}mysql-ep"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  subnet_id           = ""
+  
+  private_service_connection {
+    name                                   = "${var.prefix}${var.environment}mysql-db-endpoint"
+    is_is_manual_connection                = "false"
+    private_private_connection_resource_id = azurerm_mysql_database.sql_db.id
+    subresource_names                      = ["sqlServer"]  
+  }
+}
+
+
+
+resource "azurerm_key_vault_certificate" "ssl" {
+  name         = "mysql-cert"
+  key_vault_id = azurerm_key_vault.ss_kv.id
+
+  certificate_policy {
+    issuer_parameters {
+      name = "Self"
+    }
+
+    key_properties {
+      exportable = true
+      key_size   = 2048
+      key_type   = "RSA"
+      reuse_key  = true
+    }
+
+    lifetime_action {
+      action {
+        action_type = "AutoRenew"
+      }
+
+      trigger {
+        days_before_expiry = 30
+      }
+    }
+
+    secret_properties {
+      content_type = "application/x-pkcs12"
+    }
+
+    x509_certificate_properties {
+      # Server Authentication = 1.3.6.1.5.5.7.3.1
+      # Client Authentication = 1.3.6.1.5.5.7.3.2
+      extended_key_usage = ["1.3.6.1.5.5.7.3.1"]
+
+      key_usage = [
+        "cRLSign",
+        "dataEncipherment",
+        "digitalSignature",
+        "keyAgreement",
+        "keyCertSign",
+        "keyEncipherment",
+      ]
+
+      subject_alternative_names {
+        dns_names = ["${azurerm_sql_server.db_server.name}.mysql.database.azure.com"]
+      }
+
+      subject            = "CN=${azurerm_sql_server.db_server.name}.mysql.database.azure.com"
+      validity_in_months = 12
+    }
   }
 }
