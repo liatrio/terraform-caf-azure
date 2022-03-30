419f65d7650350e39ce6c2727e83127187beea69
Caleb Jobe <95240635+cjobee@users.noreply.github.com>
2022-03-30 17:48:48 -0500

    feat: validate app name length (#48)
    * feat: validate app name length
    
    * feat: validate app name length
    
    * refactor: updated var description

--- modules/azure/mysql-database/variables.tf
+++ modules/azure/mysql-database/variables.tf
@@ -9,6 +9,11 @@ variable "location" {
 variable "app_name" {
   type        = string
   description = "Application Name"
+
+  validation {
+    condition     = length(var.app_name) < 17
+    error_message = "Name must be less than 17 characters."
+  }
 }
 
 variable "shared_services_keyvault" {
