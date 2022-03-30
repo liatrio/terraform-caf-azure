0899e2dc1682fbb9a11cb411ddd0e3aa15bfb134
Caleb Jobe <95240635+cjobee@users.noreply.github.com>
2022-03-30 17:05:54 -0500

    refactor: removed mysql module var suffixes (#47)
    * add private endpoint
    
    Signed-off-by: Matt Reynolds <mattreynolds@Matts-MacBook-Pro.local>
    
    * refactor: removed suffixes
    
    * chore: tf fmt
    
    * chore: removed unused local
    
    * chore: removed unused local
    
    Co-authored-by: Matt Reynolds <mattreynolds@Matts-MacBook-Pro.local>

--- modules/azure/mysql-database/locals.tf
+++
@@ -1,3 +0,0 @@
-locals {
-  db_server_admin_login = "${var.app_name}-${var.environment}-db-admin"
-}
--- modules/azure/mysql-database/main.tf
+++ modules/azure/mysql-database/main.tf
@@ -29,10 +29,10 @@ resource "azurerm_key_vault_secret" "sql_pass" {
 }
 
 resource "azurerm_mysql_server" "db_server" {
-  name                             = "${var.app_name}-${var.environment}-db-server"
+  name                             = var.app_name
   location                         = var.location
   resource_group_name              = var.resource_group_name
-  administrator_login              = local.db_server_admin_login
+  administrator_login              = var.app_name
   administrator_login_password     = random_password.sql_pass.result
   ssl_minimal_tls_version_enforced = "TLS1_2"
   ssl_enforcement_enabled          = true
@@ -49,7 +49,7 @@ resource "azurerm_mysql_server" "db_server" {
 }
 
 resource "azurerm_mysql_database" "sql_db" {
-  name                = "${var.app_name}-mysql-db"
+  name                = var.app_name
   resource_group_name = var.resource_group_name
   server_name         = azurerm_mysql_server.db_server.name
   charset             = "utf8"
@@ -57,13 +57,13 @@ resource "azurerm_mysql_database" "sql_db" {
 }
 
 resource "azurerm_private_endpoint" "db_endpoint" {
-  name                = "${var.app_name}-${var.environment}-mysql-pe"
+  name                = var.app_name
   location            = var.location
   resource_group_name = var.resource_group_name
   subnet_id           = data.azurerm_subnet.snet.id
 
   private_service_connection {
-    name                           = "${var.app_name}-${var.environment}-mysql-privateserviceconnection"
+    name                           = var.app_name
     is_manual_connection           = false
     private_connection_resource_id = azurerm_mysql_server.db_server.id
     subresource_names              = ["mysqlServer"]
