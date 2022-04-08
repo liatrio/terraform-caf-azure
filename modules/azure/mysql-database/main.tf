a0813845e4967a88538342702bcf818682404e11
cjobee <calebj@liatrio.com>
2022-04-08 13:57:58 -0500

    chore: tfsec ignore

--- modules/azure/mysql-database/main.tf
+++ modules/azure/mysql-database/main.tf
@@ -29,6 +29,8 @@ resource "azurerm_key_vault_secret" "sql_pass" {
   content_type = "password"
 }
 
+#tfsec:ignore:secure-tls-policy/
+#tfsec:ignore:enable-ssl-enforcement
 resource "azurerm_mysql_server" "db_server" {
   name                             = var.app_name
   location                         = var.location
