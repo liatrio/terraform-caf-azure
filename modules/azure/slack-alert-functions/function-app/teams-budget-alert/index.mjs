fbffb50bf908c5139261171e77bcfc4538b25971
chodges7 <cdhodges5@gmail.com>
2022-04-27 15:46:26 -0700

    fix: teams function code works

--- modules/azure/slack-alert-functions/function-app/teams-budget-alert/index.mjs
+++ modules/azure/slack-alert-functions/function-app/teams-budget-alert/index.mjs
@@ -5,41 +5,27 @@ export default async function (context, req) {
     const alert = req.body;
 
     const body = {
-        "blocks": [
-            {
-                "type": "header",
-                "text": {
-                    "type": "plain_text",
-                    "text": ":exclamation: Azure Budget Alert"
-                }
-            },
-            {
-                "type": "section",
-                "fields": [
-                    {
-                        "type": "mrkdwn",
-                        "text": `*Budget Name:*\n${alert.data.BudgetName}`
-                    },
-                    {
-                        "type": "mrkdwn",
-                        "text": `*Subscription Name:*\n${alert.data.SubscriptionName}`
-                    }
-                ]
-            },
-            {
-                "type": "section",
-                "fields": [
-                    {
-                        "type": "mrkdwn",
-                        "text": `*Budgeted Amount:*\n$${alert.data.Budget}`
-                    },
-                    {
-                        "type": "mrkdwn",
-                        "text": `*Alert Threshold:*\n$${alert.data.NotificationThresholdAmount}`
-                    }
-                ]
-            }
-        ]
+        "@type": "MessageCard",
+        "@context": "http://schema.org/extensions",
+        "themeColor": "89DF00",
+        "summary": "Azure Budget Alert",
+        "sections": [{
+            "activityTitle": "Azure Budget Alert",
+            "facts": [{
+                "name": "Budget Name",
+                "value": `${alert.data.BudgetName}`
+            }, {
+               "name": "Subscription",
+               "value": `${alert.data.SubscriptionName}`
+            }, {
+                "name": "Budgeted Amount",
+                "value": `${alert.data.Budget}`
+            }, {
+                "name": "Alert Threshold",
+                "value": `${alert.data.NotificationThresholdAmount}`
+            }],
+            "markdown": true
+        }],
     }
 
     //Sending to Slack via Webhook
