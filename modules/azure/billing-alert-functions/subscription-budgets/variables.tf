1b6705f13a721293938f151294e8e435439985af
chodges7 <cdhodges5@gmail.com>
2022-04-28 09:16:01 -0700

    chore: another terraform fmt

--- modules/azure/billing-alert-functions/subscription-budgets/variables.tf
+++ modules/azure/billing-alert-functions/subscription-budgets/variables.tf
@@ -20,33 +20,33 @@ variable "default_hostname" {
 }
 
 variable "budget_amounts" {
-  type = map(any)
-  default = {"default" : 1000}
+  type    = map(any)
+  default = { "default" : 1000 }
 }
 
 variable "budget_time_grains" {
   description = "A choice of time grain, options are: Annually, BillingAnnual, BillingMonth, BillingQuarter, Monthly, and Quarterly"
-  type = map(any)
-  default = {"default" : "Monthly"}
+  type        = map(any)
+  default     = { "default" : "Monthly" }
 }
 
 variable "budget_operator" {
   description = "A choice of operator, options are: EqualTo, GreaterThan, or GreaterThanOrEqualTo"
-  type = map(any)
-  default = {"default" = "EqualTo"}
+  type        = map(any)
+  default     = { "default" = "EqualTo" }
 }
 
 variable "budget_threshold" {
-  type = map(any)
-  default = {"default" = 80.0}
+  type    = map(any)
+  default = { "default" = 80.0 }
 }
 
 variable "enable_slack" {
-  type = bool
+  type    = bool
   default = false
 }
 
 variable "enable_teams" {
-  type = bool
+  type    = bool
   default = false
 }
