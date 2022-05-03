78b315305b618faea4e4831a79b26d98c7777906
Adriel Perkins <adrielp@liatrio.com>
2022-05-03 15:53:53 -0400

    fix: ENG-2227 fix count evaluating empty map and output value (#84)

--- modules/azure/key-gen/outputs.tf
+++ modules/azure/key-gen/outputs.tf
@@ -1,3 +1,3 @@
 output "disk_encryption_set_id" {
-  value = azurerm_disk_encryption_set.encryption_set[0].id
+  value = length(azurerm_disk_encryption_set.encryption_set) > 0 ? azurerm_disk_encryption_set.encryption_set[0].id : null
 }
