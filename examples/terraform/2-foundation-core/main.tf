adc4b7619df76088a4d424d1bb02eabd86a81b4d
Anthony Gauthier <antho325@hotmail.com>
2022-05-04 22:11:38 -0400

    refactor: added environment to example tf file

--- examples/terraform/2-foundation-core/main.tf
+++ examples/terraform/2-foundation-core/main.tf
@@ -36,6 +36,7 @@ module "liatrio_caf_foundation" {
     features = "caf-example_root_dns"
   }
   prefix                               = "example"
+  environment                          = "dev"
   virtual_hub_address_cidr             = "10.130.0.0/23"
   vpn_client_pool_address_cidr         = "10.130.2.0/24"
   connectivity_apps_address_cidr       = "10.130.3.0/24"
