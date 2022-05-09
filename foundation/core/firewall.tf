c9855e417553542d9b48b5691fa38135beb2ff15
Caleb Jobe <95240635+cjobee@users.noreply.github.com>
2022-05-09 08:44:47 -0500

    feat: Azure Firewall flag and DNS proxy (#87)
    * feat: firewall flag
    
    * refactor: added toList
    
    * reafactor: renamed resource
    
    * Update foundation/core/outputs.tf
    
    Co-authored-by: Justin Bankes <12632465+jbankes@users.noreply.github.com>
    
    * Update foundation/core/firewall.tf
    
    Co-authored-by: Justin Bankes <12632465+jbankes@users.noreply.github.com>
    
    * refactor: fixing validate issue
    
    * refactor: fixing validate issue
    
    * fix: added element to vpn dns server
    
    Co-authored-by: Justin Bankes <12632465+jbankes@users.noreply.github.com>

---
+++ foundation/core/firewall.tf
@@ -0,0 +1,36 @@
+resource "azurerm_firewall_policy" "firewall_policy" {
+  count = var.enable_firewall ? 1 : 0
+
+  name                = "afwp-${var.prefix}-${azurerm_resource_group.caf_connectivity.location}"
+  provider            = azurerm.connectivity
+  resource_group_name = azurerm_resource_group.caf_connectivity.name
+  location            = var.location
+  dns {
+    proxy_enabled = true
+    servers       = [module.vpn_dns_resolver[0].ip_address]
+  }
+}
+
+resource "azurerm_firewall_policy_rule_collection_group" "rule_collection_group" {
+  count = var.enable_firewall == true ? 1 : 0
+
+  provider           = azurerm.connectivity
+  name               = "wafrg-${var.prefix}-${azurerm_resource_group.caf_connectivity.location}"
+  firewall_policy_id = azurerm_firewall_policy.firewall_policy[0].id
+  priority           = 500
+}
+
+resource "azurerm_firewall" "firewall" {
+  count = var.enable_firewall == true ? 1 : 0
+
+  provider           = azurerm.connectivity
+  name               = "afw-${var.prefix}-${azurerm_resource_group.caf_connectivity.location}"
+  location           = var.location
+  sku_name           = "AZFW_Hub"
+  firewall_policy_id = azurerm_firewall_policy.firewall_policy[0].id
+  threat_intel_mode  = ""
+  virtual_hub {
+    virtual_hub_id = azurerm_virtual_hub.caf_hub.id
+  }
+  resource_group_name = azurerm_resource_group.caf_connectivity.name
+}
--- foundation/core/variables.tf
+++ foundation/core/variables.tf
@@ -104,10 +104,10 @@ variable "enable_ms_defender" {
   default     = false
 }
 
-variable "firewall_sku" {
-  type        = string
-  description = "AZFW_Hub or AZFW_VNet"
-  default     = "AZFW_Hub"
+variable "enable_firewall" {
+  type        = bool
+  description = "Enables the firewall and its association with the virtual hub"
+  default     = false
 }
 
 variable "enable_point_to_site_vpn" {
