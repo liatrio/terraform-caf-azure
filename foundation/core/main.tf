07748c146fd9f7be6469d13bd65be99f40d7b603
DJTJWillia <williams.tjoseph@gmail.com>
2022-05-10 14:09:26 -0600

    feat: upgrading final references to 2.96 -> 3.5

--- foundation/core/main.tf
+++ foundation/core/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.96"
+      version = "~> 3.5.0"
       configuration_aliases = [
         azurerm.default,
         azurerm.identity,
