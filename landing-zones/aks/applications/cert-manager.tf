8ae1880fb83e8702abfc606b10958274601ad876
cjobee <calebj@liatrio.com>
2022-05-19 09:17:40 -0500

    feat: add private workflow support to aks lz by default

--- landing-zones/aks/applications/cert-manager.tf
+++ landing-zones/aks/applications/cert-manager.tf
@@ -45,6 +45,21 @@ module "external_issuer" {
   dns_zone_name         = var.dns_zone_name
 }
 
+module "internal_issuer" {
+  depends_on = [
+    time_sleep.wait_for_cert_manager,
+  ]
+  source = "../../../modules/kubernetes/cert-manager-issuer"
+
+  namespace             = kubernetes_namespace.toolchain_namespace.metadata.0.name
+  issuer_name           = "internal-issuer"
+  issuer_server         = var.issuer_server
+  issuer_email          = var.issuer_email
+  azure_subscription_id = var.azure_subscription_id
+  resource_group_name   = var.dns_zone_resource_group_name
+  dns_zone_name         = local.internal_dns_zone_name
+}
+
 module "external_wildcard" {
   source = "../../../modules/kubernetes/certificates"
 
@@ -55,4 +70,14 @@ module "external_wildcard" {
   domain    = var.dns_zone_name
 
   issuer_name = module.external_issuer[0].issuer_name
+}
+
+module "internal_wildcard" {
+  source = "../../../modules/kubernetes/certificates"
+
+  name      = "internal-wildcard"
+  namespace = kubernetes_namespace.toolchain_namespace.metadata.0.name
+  domain    = local.internal_dns_zone_name
+
+  issuer_name = module.internal_issuer.issuer_name
 }
--- landing-zones/aks/applications/ingress.tf
+++ landing-zones/aks/applications/ingress.tf
@@ -9,3 +9,12 @@ module "external_ingress_controller" {
   ingress_class       = "nginx"
   default_certificate = module.external_wildcard[0].cert_secret_name
 }
+
+module "internal_ingress_controller" {
+  source              = "../../../modules/kubernetes/ingress-controller"
+  internal            = true
+  name                = "internal"
+  namespace           = kubernetes_namespace.toolchain_namespace.metadata.0.name
+  default_certificate = module.internal_wildcard.cert_secret_name
+  ingress_class       = "nginx-internal"
+}
