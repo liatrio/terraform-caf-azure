38e562a7c7bdedb6bc666d96b2226d345a738c4b
Caleb Jobe <95240635+cjobee@users.noreply.github.com>
2022-05-20 09:43:18 -0500

    feat: add internal workflow support to aks lz by default (#94)
    * feat: upgrading to AzureRM 3.5
    
    * feat: upgrading final references to 2.96 -> 3.5
    
    * fix: readding the lock files
    
    * fix: 3.5 update - cap fixes
    
    * fix: 3.5 upgrade requires sky_tier
    
    * fix: 3.5 upgrade, threat intel mode is now Alert
    
    * fix: aks fixes for version 3.5
    
    * fix: correcting toggle, and adding data.tf files
    
    * fix: ensuring dns is built in foundation/core
    
    * fix: sensitive values
    
    * feat: moving shared-services to the top level'
    
    * fix: moving Shared Services back
    
    * updating aks cluster and lock files
    
    * fix: updating node resource group name
    
    * feat: add private workflow support to aks lz by default
    
    * refactor: renamed resource
    
    * refactor: rename managed id resource
    
    Co-authored-by: DJTJWillia <williams.tjoseph@gmail.com>

--- landing-zones/aks/applications/external-dns.tf
+++ landing-zones/aks/applications/external-dns.tf
@@ -1,8 +1,6 @@
 module "external_dns_pod_identity" {
   source = "../../../modules/kubernetes/aad-pod-identity-instance"
 
-  count = var.external_app ? 1 : 0
-
   namespace = kubernetes_namespace.toolchain_namespace.metadata.0.name
 
   identity_name        = "external-dns-pod-identity"
@@ -25,4 +23,19 @@ module "public" {
   tenant_id                    = data.azurerm_client_config.current.tenant_id
   azure_subscription_id        = var.azure_subscription_id
   release_name                 = "external-dns-public"
+}
+
+module "external_dns_private" {
+  source = "../../../modules/kubernetes/external-dns"
+
+  pod_identity = module.external_dns_pod_identity.identity_name
+  dns_provider = "azure-private-dns"
+  domain_filters = [
+    "${local.internal_dns_zone_name}"
+  ]
+  namespace                    = kubernetes_namespace.toolchain_namespace.metadata.0.name
+  dns_zone_resource_group_name = var.dns_zone_resource_group_name
+  tenant_id                    = data.azurerm_client_config.current.tenant_id
+  azure_subscription_id        = var.azure_subscription_id
+  release_name                 = "external-dns-private"
 }
