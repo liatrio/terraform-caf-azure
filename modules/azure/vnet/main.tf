26f8f0ffcdcd57ba279f57382b6107532d27bdc0
DJTJWillia <williams.tjoseph@gmail.com>
2022-05-25 15:53:01 -0600

    fix: wrong vnet name

--- modules/azure/vnet/main.tf
+++ modules/azure/vnet/main.tf
@@ -46,8 +46,8 @@ resource "azurerm_virtual_network" "vnet" {
 
 resource "azurerm_subnet" "subnet" {
   name                 = var.name
-  resource_group_name  = azurerm_virtual_network.aks_vnet.resource_group_name
-  virtual_network_name = azurerm_virtual_network.aks_vnet.name
+  resource_group_name  = azurerm_virtual_network.vnet.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = [var.vnet_address_range]
 
   # This needs to be enabled for the kube-apiserver endpoint to be created
