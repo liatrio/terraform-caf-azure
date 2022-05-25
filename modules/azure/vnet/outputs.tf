48477744b00fe0648215b892788d6f891ce80794
DJTJWillia <williams.tjoseph@gmail.com>
2022-05-25 15:08:43 -0600

    fix: outputs

--- modules/azure/vnet/outputs.tf
+++ modules/azure/vnet/outputs.tf
@@ -1,7 +1,7 @@
-output "vnet_subnet_id" {
-  value = azurerm_virtual_network.vnet.subnet.id
-}
-
 output "vnet_id" {
   value = azurerm_virtual_network.vnet.id
+}
+
+output "vnet_subnet" {
+  value = azurerm_virtual_network.vnet.subnet
 }
