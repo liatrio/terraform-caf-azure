e79d6d8cf3a1dc3e7152262dcfacc5cc71ac862b
Chris Schreiber <chriss@liatrio.com>
2022-06-13 10:37:57 -0700

    fix: turn on logging for storage account

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -10,6 +10,15 @@ resource "azurerm_storage_account" "terraformstate" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
   min_tls_version          = "TLS1_2"
+  queue_properties {
+    logging {
+      delete                = true
+      read                  = true
+      write                 = true
+      version               = "1.0"
+      retention_policy_days = 10
+    }
+  }
 
   tags = {}
 }
