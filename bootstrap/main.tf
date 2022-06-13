c8edcedc08ebc0593f161dceeb00d2e3c895651f
Chris Schreiber <chriss@liatrio.com>
2022-06-13 10:32:14 -0700

    chore: add tfstat bootstrap

---
+++ bootstrap/main.tf
@@ -0,0 +1,20 @@
+resource "azurerm_resource_group" "terraformstate" {
+  name     = "terraform"
+  location = var.location
+}
+
+resource "azurerm_storage_account" "terraformstate" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.terraformstate.name
+  location                 = azurerm_resource_group.terraformstate.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {}
+}
+
+resource "azurerm_storage_container" "terraformstate" {
+  name                  = "terraform-caf-azure-liatrio"
+  storage_account_name  = azurerm_storage_account.terraformstate.name
+  container_access_type = "private"
+}
