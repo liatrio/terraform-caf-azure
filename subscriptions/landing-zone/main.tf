2cc55b9cf997f4c71eff4b5079d83fcd03448fa6
Blair Drummond <blaird@liatrio.com>
2022-06-16 15:56:32 -0400

    fix: use resource instead of data

--- subscriptions/landing-zone/main.tf
+++ subscriptions/landing-zone/main.tf
@@ -20,9 +20,13 @@ resource "azurerm_subscription" "landing_zone" {
   tags = {}
 }
 
-data "azurerm_management_group" "landing_zone_mg" {
+resource "azurerm_management_group" "landing_zone_mg" {
   name = var.management_group_name
 }
+
+#data "azurerm_management_group" "landing_zone_mg" {
+#  name = var.management_group_name
+#}
 
 # This is because the `azurerm_subscription` resource's `id` is actually an alias ID, not a subscription ID
 data "azurerm_subscription" "landing_zone" {
@@ -31,5 +35,5 @@ data "azurerm_subscription" "landing_zone" {
 
 resource "azurerm_management_group_subscription_association" "landing_zone_mg_association" {
   subscription_id     = data.azurerm_subscription.landing_zone.id
-  management_group_id = data.azurerm_management_group.landing_zone_mg.id
+  management_group_id = azurerm_management_group.landing_zone_mg.id
 }
