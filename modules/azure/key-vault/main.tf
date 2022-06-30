ac10a07ab70ca92d21ac05997942d6321505ccbf
Blair Drummond <blaird@liatrio.com>
2022-06-30 15:52:16 -0400

    chore: bump azurerm in keyvault

--- modules/azure/key-vault/main.tf
+++ modules/azure/key-vault/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.5.0"
+      version = "~> 3.11.0"
       configuration_aliases = [
         azurerm.connectivity
       ]
